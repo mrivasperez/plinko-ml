68ac7e84a552dd2926d7f5d1297d421e7f706571
migue <mrivasperez@icloud.com>
2024-04-02 08:54:29 -0700

    chore: Comment prototyping code

--- score.js
+++ score.js
@@ -24,11 +24,15 @@ const predictionDroppoint = 300;
 // the k value - how many "neighbors" will be checked
 const kPoint = 3;
 
+// calculate the distance from the droppoint to the 'predictionDroppoint'
 const distanceFromPredictionDroppoint = (point) => {
   return Math.abs(point - predictionDroppoint);
 };
 
 _.chain(outputs)
+  // map through the results to create a new array: [dropPoint, bucket][]
   .map((result) => [distanceFromPredictionDroppoint(result[0], result[3])])
+  // sort the array by distance from prediction droppoint
   .sortBy((row) => row[0])
+  // slice the sorted array up to the kPoint (nearest neighbors to consider)
   .slice(0, kPoint);
